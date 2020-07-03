54200a1636dc3394fe9949da8964327f8606c0b5
yangzhao <yangzhao@mcalo.com>
2020-07-03 21:09:04 +0800

    update

---
+++ contracts/storage/FundStorage.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.6.10;
+
+import "../interface/IPerpetual.sol";
+
+contract FundStorage is ERC20Storage {
+    // underlaying perpetual.
+    address internal _collateral;
+    uint256 internal _scaler;
+    // state
+    uint256 internal _totalFeeClaimed;
+    uint256 internal _maxNetAssetValue;
+    uint256 internal _lastPassiveClaimingTime;
+    uint256 internal _lastActiveClaimingTime;
+
+    // configurations
+    uint256 internal _redeemingLockdownPeriod;
+    uint256 internal _feeClaimingPeriod;
+    uint256 internal _minimalRedeemingPeriod;
+    uint256 internal _entranceFeeRate;
+    uint256 internal _streamingFeeRate;
+    uint256 internal _performanceFeeRate;
+
+    // accounts
+    mapping(address => uint256) internal _redeemingBalances;
+    mapping(address => uint256) internal _lastPurchaseTime;
+
+    // dependencies
+    address internal _maintainer;
+    IPerpetual internal _perpetual;
+}
