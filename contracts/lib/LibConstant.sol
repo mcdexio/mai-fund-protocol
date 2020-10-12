d818ab75988a52f8202543987f193f708db5412c
yangzhao <yangzhao@mcalo.com>
2020-10-12 16:44:00 +0800

    [N14] Unused constant

--- contracts/lib/LibConstant.sol
+++ contracts/lib/LibConstant.sol
@@ -5,7 +5,6 @@ library LibConstant {
     int256 internal constant SIGNED_ONE = 10 ** 18;
     uint256 internal constant UNSIGNED_ONE = 10 ** 18;
 
-    uint256 internal constant SHARE_TOKEN_DECIMALS = 18;
     uint256 internal constant MAX_COLLATERAL_DECIMALS = 18;
 
     uint256 internal constant SECONDS_PER_YEAR = 365 * 86400;
