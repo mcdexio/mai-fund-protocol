766c5b7ead8f58ff8acee6e0b1e948b6d919c830
yangzhao <yangzhao@mcalo.com>
2020-10-12 16:36:27 +0800

    [N10] LibTypes is unused as a library

--- contracts/fund/implement/AutoTradingFund.sol
+++ contracts/fund/implement/AutoTradingFund.sol
@@ -140,7 +140,7 @@ contract AutoTradingFund is
         require(needRebalance, "need no rebalance");
         require(targetSide == side, "unexpected side");
         uint256 tradingAmount = Math.min(maxPositionAmount, targetAmount);
-        ( uint256 tradingPrice, ) = _biddingPrice(LibTypes.opposite(targetSide), priceLimit, _rebalanceSlippage);
+        ( uint256 tradingPrice, ) = _biddingPrice(targetSide.opposite(), priceLimit, _rebalanceSlippage);
         _tradePosition(_self(), _msgSender(), targetSide, tradingPrice, tradingAmount);
         emit Rebalance(targetSide, tradingPrice, tradingAmount);
     }
