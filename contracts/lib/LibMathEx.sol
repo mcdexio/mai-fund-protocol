daa9467cec85dba08b86e93fcb78f8eca18972c1
zhutianchi <zhutianchi@mcarlo.com>
2020-10-12 21:20:55 +0800

    add RoundHalfUp

--- contracts/lib/LibMathEx.sol
+++ contracts/lib/LibMathEx.sol
@@ -25,16 +25,24 @@ library LibMathEx {
     }
 
     function wmul(int256 x, int256 y) internal pure returns (int256 z) {
-        z = x.mul(y).add(LibConstant.SIGNED_ONE / 2) / LibConstant.SIGNED_ONE;
+        z = roundHalfUp(x.mul(y), LibConstant.SIGNED_ONE) / LibConstant.SIGNED_ONE;
     }
 
     function wdiv(int256 x, int256 y) internal pure returns (int256 z) {
-        z = x.mul(LibConstant.SIGNED_ONE).add(y / 2).div(y);
+        if (y < 0) {
+            y = -y;
+            x = -x;
+        }
+        z = roundHalfUp(x.mul(LibConstant.SIGNED_ONE), y) / y;
     }
 
     function wfrac(int256 x, int256 y, int256 z) internal pure returns (int256 r) {
-        require(z != 0, "division by zero");
-        r = x.mul(y).div(z);
+        int256 t = x.mul(y);
+        if (z < 0) {
+            z = neg(z);
+            t = neg(t);
+        }
+        r = roundHalfUp(t, z) / z;
     }
 
     function abs(int256 x) internal pure returns (int256) {
@@ -45,4 +53,13 @@ library LibMathEx {
         return SignedSafeMath.sub(int256(0), a);
     }
 
+    // ROUND_HALF_UP rule helper. You have to call roundHalfUp(x, y) / y to finish the rounding operation
+    // 0.5 ≈ 1, 0.4 ≈ 0, -0.5 ≈ -1, -0.4 ≈ 0
+    function roundHalfUp(int256 x, int256 y) internal pure returns (int256) {
+        require(y > 0, "roundHalfUp only supports y > 0");
+        if (x >= 0) {
+            return x.add(y / 2);
+        }
+        return x.sub(y / 2);
+    }
 }
