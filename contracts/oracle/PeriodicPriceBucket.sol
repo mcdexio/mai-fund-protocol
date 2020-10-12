4ab347871114b8e76cd565c30386263a8bd67619
yangzhao <yangzhao@mcalo.com>
2020-10-12 16:21:01 +0800

    [N06] require checks modify state

--- contracts/oracle/PeriodicPriceBucket.sol
+++ contracts/oracle/PeriodicPriceBucket.sol
@@ -89,7 +89,8 @@ contract PeriodicPriceBucket is OwnableUpgradeSafe {
         require(period <= 86400*7, "period must be less than 1 week");
         require(period > 0, "period must be greater than 0");
         require(_periods.length() < MAX_BUCKETS, "number of buckets reaches limit");
-        require(_periods.add(period), "period is duplicated");
+        bool success = _periods.add(period);
+        require(success, "period is duplicated");
         emit AddBucket(period);
     }
 
