3ce542080092ffc9787a0642246e9b9a57cbca0f
yangzhao <yangzhao@mcalo.com>
2020-10-12 16:24:54 +0800

    [N08] IAggregator interface name is not consistent

--- contracts/interface/IAggregator.sol
+++ contracts/interface/IAggregator.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.6.10;
 
-interface AggregatorInterface {
+interface IAggregator {
     function latestAnswer() external view returns (int256);
     function latestTimestamp() external view returns (uint256);
     function latestRound() external view returns (uint256);
