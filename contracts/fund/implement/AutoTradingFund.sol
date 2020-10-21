7ca1cf75a4b02dffe7d2a1e1d420f9603e939443
yangzhao <yangzhao@mcalo.com>
2020-10-21 11:31:30 +0800

    add default value for amount and side

--- contracts/fund/implement/AutoTradingFund.sol
+++ contracts/fund/implement/AutoTradingFund.sol
@@ -119,6 +119,9 @@ contract AutoTradingFund is
                 currentNetAssetValue,
                 nextTargetLeverage
             );
+        } else {
+            amount = 0;
+            side = LibTypes.Side.FLAT;
         }
     }
 
